9ba2d8e6e74bca4f9cc680a239cf9b65aed190b3
Abhirami <137210545+abhirami102@users.noreply.github.com>
2026-05-28 02:36:15 +0530

    Refactor FCFS code for better readability

--- fcfs.c
+++ fcfs.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 int main() {
-    int n, at[20], bt[20], ct=0, tat, wt;
-    float total_wt=0, total_tat=0;
-
-    printf("Enter n: "); scanf("%d", &n);
+    int n, at[20], bt[20], ct = 0, tat, wt;
+    float total_wt = 0, total_tat = 0;
+    
+    printf("Enter n: "); 
+    scanf("%d", &n);
     printf("Enter AT and BT:\n");
-    for(int i=0; i<n; i++) scanf("%d %d", &at[i], &bt[i]);
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &at[i], &bt[i]);
+    }
 
     printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
-    for(int i=0; i<n; i++) {
-        if(ct < at[i]) ct = at[i]; // CPU Idle handling
+    for (int i = 0; i < n; i++) {
+        if (ct < at[i]) {
+            ct = at[i]; 
+        }
         ct += bt[i];
         tat = ct - at[i];
         wt = tat - bt[i];
-        total_wt += wt; total_tat += tat;
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i+1, at[i], bt[i], ct, tat, wt);
+        total_wt += wt; 
+        total_tat += tat;
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], ct, tat, wt);
     }
-    printf("\nAvg WT = %.2f, Avg TAT = %.2f\n", total_wt/n, total_tat/n);
+    printf("\nAvg WT = %.2f, Avg TAT = %.2f\n", total_wt / n, total_tat / n);
     return 0;
 }
